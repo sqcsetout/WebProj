46bcaa2e09005c76a76256e94c9bde14d8303cd9
shenqichen <shenqichen@qiyi.com>
2019-02-11 17:58:38 +0800

    1.3.1

--- djsite/blog/admin.py
+++ djsite/blog/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import BlogArticles
 
-admin.site.register(BlogArticles)
+class BlogArticlesAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "publish")
+    list_filter = ("publish", "author")
+    search_fields = ("title", "body")
+    date_hierarchy = "publish"
+    ordering = ["publish", "author"]
+    
+admin.site.register(BlogArticles, BlogArticlesAdmin)
