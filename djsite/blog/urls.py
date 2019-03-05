74fa66bbc1209577c04e61fcdc171ad785d063db
shenqichen <shenqichen@stevendeMacBook-Pro.local>
2019-03-05 23:54:34 +0800

    chapter2.1 end

--- djsite/blog/urls.py
+++ djsite/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, re_path
 from . import views
 
+app_name= 'blog'
 urlpatterns = [
     re_path(r'^$', views.blog_title, name='blog_title'),
     re_path(r'^(?P<article_id>\d)/$', views.blog_article, name='blog_detail')
